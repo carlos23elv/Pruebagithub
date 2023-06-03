1afc81501a1ef6f5a1efc18bd8852391490752e0
carlos23elv <74583299+carlos23elv@users.noreply.github.com>
2023-06-03 10:21:03 -0500

    Update Pruebagithub.java

--- src/pruebagithub/Pruebagithub.java
+++ src/pruebagithub/Pruebagithub.java
@@ -14,7 +14,7 @@ public class Pruebagithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        System.out.println("prueba 12354");
     }
     
 }
